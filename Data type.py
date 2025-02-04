45527de22f0c6b6b4d30a040628f842d98fd924b
Alusa-a <barrackalusa@gmail.com>
2025-02-04 16:21:12 +0300

    initial commit

---
+++ Data type.py
@@ -0,0 +1,30 @@
+number=50#Int
+num=56.98#float
+greeting="Hello there"#str
+hasfeathers=True#Boolean
+
+#Data Structures
+cars=["Mercedes", "Toyota", "Ferrari"]#list
+fruits=("Banana", "Orange" ,"Apple")#Tuple
+countries={"England", "France", "Germany"}#set
+student={
+    "firstname" : "Anne",
+    "course" : "MIT",
+    "age" : 19,
+    "nationality" : "Kenyan"
+}
+
+print(number)
+print(num)
+print(greeting)
+print(hasfeathers)
+print(cars)
+print(fruits)
+print(countries)
+print(student)
+print(student["nationality"])
+
+
+#Typecasting-Converting one data type to another
+print(float(number))
+print(int(num))
